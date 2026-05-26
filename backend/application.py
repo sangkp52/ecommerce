e459c9090935b88f65e35803db1f86d15ccd4f42
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-27 05:57:19 +0700

    fix bugs

--- backend/application.py
+++ backend/application.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from routers.users import router as users_routers
-from routers.products import router as products_routers
-from middlewares.cors import apply_cors_middleware
-from routers import api_router
-from auth.router import router as auth_router
+
+# from routers.users import router as users_routers
+# from routers.products import router as products_routers
+# from middlewares.cors import apply_cors_middleware
+# from routers import api_router
+# from auth.router import router as auth_router
+
+from src.routers.users import router as users_routers
+from src.routers.products import router as products_routers
+from src.middlewares.cors import apply_cors_middleware
+from src.routers import api_router
+from src.auth.router import router as auth_router
 
 def create_app() -> FastAPI:
     app = FastAPI()
@@ -14,5 +21,7 @@ def create_app() -> FastAPI:
     # Configuring routers
     app.include_router(api_router)
     app.include_router(auth_router)
-    
+    app.include_router(users_routers)
+    app.include_router(products_routers)
+
     return app
