261ad31d9dc0ef9302959972d4baa77d8334f01a
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-27 06:10:26 +0700

    fix bugs

--- backend/src/middlewares/cors.py
+++ backend/src/middlewares/cors.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from config import config
+# from config import config
+from src.config import config
 
 origins = config.get("CORS", "").split(",")
 print("cors: ", origins)
