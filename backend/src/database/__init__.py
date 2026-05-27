1437ea43b391c842a901754a89370bdf45eb2948
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-27 07:12:52 +0700

    test

--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,7 +1,8 @@
 import os
 import motor.motor_asyncio
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
+MONGO_URI: mongodb://admin:admin123@mongo:27017/ecommerce?authSource=admin
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client.get_database("ecommerce")
