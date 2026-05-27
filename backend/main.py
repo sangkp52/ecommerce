b14034ff7a8550220fe44dfcb4941eb309b5cbb1
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-27 09:16:45 +0700

    test deploy

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from application import create_app
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = create_app()
 
 # if __name__ == "__main__":
 #     app.run()
+
+app = FastAPI()
+Instrumentator().instrument(app).expose(app)
