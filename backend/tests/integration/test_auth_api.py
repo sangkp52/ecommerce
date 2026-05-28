58ed61777bad4b75e1614e79844e78b309c6fb5f
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-28 11:10:32 +0700

    test git

--- backend/tests/integration/test_auth_api.py
+++ backend/tests/integration/test_auth_api.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from application import create_app
+from httpx import AsyncClient
 
 @pytest.fixture
 def client():
@@ -22,11 +23,16 @@ def test_signup(client):
     assert response.status_code == 200
     assert "email" in response.json()
 
+@pytest.mark.asyncio
+async def test_login():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post("/auth/login", json={"email": "test@example.com", "password": "123456"})
+        assert response.status_code == 200
 
-def test_login(client):
-    response = client.post("/auth/login", json={
-        "email": "test@example.com",
-        "password": "123456"
-    })
-    assert response.status_code == 200
-    assert "access_token" in response.json()
+# def test_login(client):
+#     response = client.post("/auth/login", json={
+#         "email": "test@example.com",
+#         "password": "123456"
+#     })
+#     assert response.status_code == 200
+#     assert "access_token" in response.json()
