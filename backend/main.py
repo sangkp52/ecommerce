97cd0d37c51c2af3757a1a6a95e8b9fed09ac0e1
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-29 07:06:03 +0700

    fix deploy

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,16 @@
 from fastapi import FastAPI
 from application import create_app
 from prometheus_fastapi_instrumentator import Instrumentator
+
 from src.routers import api_router, root_router
 from src.auth.router import router as auth_router
 
 app = create_app()
 
-# if __name__ == "__main__":
-#     app.run()
+# Register routers
+app.include_router(api_router)
+app.include_router(root_router)
+app.include_router(auth_router)
 
+# Prometheus metrics
 Instrumentator().instrument(app).expose(app)
-# app.include_router(api_router)
-# app.include_router(root_router)
-# app.include_router(auth_router)
-
-REQUEST_COUNT = Counter(
-    "http_requests_total",
-    "Total HTTP requests",
-    ["method", "endpoint"]
-)
-
-REQUEST_LATENCY = Histogram(
-    "http_request_latency_seconds",
-    "Request latency"
-)
-
-@app.middleware("http")
-async def monitor_requests(request: Request, call_next):
-    start = time.time()
-
-    response = await call_next(request)
-
-    duration = time.time() - start
-
-    REQUEST_COUNT.labels(request.method, request.url.path).inc()
-    REQUEST_LATENCY.observe(duration)
-
-    return response
-
-
-@app.get("/metrics")
-def metrics():
-    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
