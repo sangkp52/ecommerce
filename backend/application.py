3a33a2a9f3a71212f40b16513846df5e8e967470
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-29 07:35:40 +0700

    fix tests

--- backend/application.py
+++ backend/application.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from src.routers import api_router, root_router
 from src.auth.router import router as auth_router
 from src.routers.users import router as users_routers
@@ -16,5 +18,11 @@ def create_app() -> FastAPI:
     app.include_router(auth_router)
     app.include_router(users_routers)
     app.include_router(products_routers)
+
+    Instrumentator().instrument(app).expose(
+        app,
+        endpoint="/metrics",
+        include_in_schema=False
+    )
 
     return app
--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,24 @@
-from fastapi import FastAPI
-from application import create_app
-from prometheus_fastapi_instrumentator import Instrumentator
+# from fastapi import FastAPI
+# from application import create_app
+# from prometheus_fastapi_instrumentator import Instrumentator
 
-from src.routers import api_router, root_router
-from src.auth.router import router as auth_router
+# from src.routers import api_router, root_router
+# from src.auth.router import router as auth_router
+
+# app = create_app()
+
+# # Register routers
+# app.include_router(api_router)
+# app.include_router(root_router)
+# app.include_router(auth_router)
+
+# # Prometheus metrics
+# Instrumentator().instrument(app).expose(
+#     app,
+#     endpoint="/metrics",
+#     include_in_schema=False
+# )
+
+from application import create_app
 
 app = create_app()
-
-# Register routers
-app.include_router(api_router)
-app.include_router(root_router)
-app.include_router(auth_router)
-
-# Prometheus metrics
-Instrumentator().instrument(app).expose(
-    app,
-    endpoint="/metrics",
-    include_in_schema=False
-)
