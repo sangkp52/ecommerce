34ccc5544d14595bc3576370b54929730325d530
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-29 10:25:19 +0700

    change smth

--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,9 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27018")
+MONGO_URI = os.getenv("MONGO_URI") 
 
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["ecommerce"]
 
 def get_db():
-    client = AsyncIOMotorClient(MONGO_URI)
-    return client["ecommerce"]
+    return db
