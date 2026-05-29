988acdea97a4e22a537baf38037a2465e99009f7
sangkp52 <huynhphuocsang21091978@gmail.com>
2026-05-29 09:26:47 +0700

    fix sonar

--- backend/src/database/__init__.py
+++ backend/src/database/__init__.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27018")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 
 def get_db():
